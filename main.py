1abf3ee63681c036d36dd95c3c108dee57a5eb3f
songdohyeok <songdh801@naver.com>
2025-04-17 14:12:03 +0900

    Update main

--- main.py
+++ main.py
@@ -9,4 +9,7 @@ def home():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", reload=True, # 코드변경시 서버에 바로 적용
+                host="0.0.0.0",          # 0.0.0.0 외부 접속 허용
+                port=8000)               # 서버 포트를 8000번 사용 
+                                         # (포트 바꾸면 ec2 인바운드 규칙 고려 포트 열어놓기)
